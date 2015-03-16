8cb7aa84001ccd0bd206e425aada34878cb85df6
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-03-16 13:31:09 +0100

    QUALI-55 #time 45m fix provisioning for vagrant

--- vagrant/puphpet/files/startup-always/setup.sh
+++ vagrant/puphpet/files/startup-always/setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # vars
 activator_version=1.3.2
@@ -10,26 +10,24 @@ project_home="/quali-t-app"
 # sudo apt-get upgrade
 # . ~/.bashrc
 # sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-# 
-# # Required for SASS
-# sudo `which gem` install compass
-# 
-# # save pgpassword in environment
-# echo "source PGPASSWORD=123 >> /home/vagrant/.bashrc
-# 
-# sql -U postgres -h localhost -c 'ALTER USER qualitapp CREATEDB;'
+
+# Required for SASS
+sudo `which gem` install compass
+
+# save pgpassword in environment
+echo "source PGPASSWORD=123" >> /home/vagrant/.bashrc
+
+sql -U postgres -h localhost -c "ALTER USER qualitapp CREATEDB;"
 
 # Download activator
-cd ~
-sudo wget $activator_download_url_min
 cd $vagrant_home
-sudo unzip $vagrant_home/typesafe-activator-$activator_version-minimal.zip
-sudo rm $vagrant_home/typesafe-activator-$activator_version-minimal.zip
+sudo wget $activator_download_url_min
+sudo unzip $vagrant_home/typesafe-activator-$activator_version-minimal.zip 
+sudo chmod +x -R $vagrant_home/activator-$activator_version-minimal/*
+sudo rm $vagrant_home/typesafe-activator-$activator_version-minimal.zip*
 
 # add activator to path
 echo "export PATH=$vagrant_home/activator-$activator_version-minimal:\$PATH" >> ~/.bashrc
 
 #reset bash
-# source ~/.bashrc
-
 . ~/.bashrc
