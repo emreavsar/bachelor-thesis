12fca9c9a008bda77d3209aeefdf2d5439ad1cff
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-03-17 12:48:34 +0100

    QUALI-66 #time 30m Cleanup frontend app

---
+++ quali-t-app/public/app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qualitApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the qualitApp
+ */
+angular.module('qualitApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- quali-t-app/public/test/spec/controllers/about.js
+++ quali-t-app/public/test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('publicApp'));
+  beforeEach(module('qualitApp'));
 
   var AboutCtrl,
     scope;
