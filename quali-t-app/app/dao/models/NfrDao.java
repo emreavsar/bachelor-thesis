9b0fc85093282fb495d08c655f38817ca8ace10b
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-03-25 15:29:59 +0100

    prepare for construction phase

--- quali-t-app/app/dao/models/NfrDao.java
+++ quali-t-app/app/dao/models/NfrDao.java
@@ -7,9 +7,4 @@ import models.Nfr;
  * Created by emre on 16/03/15.
  */
 public class NfrDao extends AbstractDAO<Nfr> {
-    /*
-    public Nfr readById(Long id) {
-        return find("select n from Nfr n where n.id = ?", id);
-    }
-    */
 }
--- quali-t-app/conf/evolutions/default/2.sql
+++
@@ -1,9 +0,0 @@
-# --- !Ups
-
--- Creating NFRs
-INSERT INTO nfr ( ID , DESCRIPTION ) VALUES (nextval('entity_seq'), 'Das Kassen-System für die Abrechnung muss 99.99% im Jahr verfügbar sein');
-
-
-# --- !Downs
-
-delete from nfr;
