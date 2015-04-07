2e624ccef65a5df2d3c0000ccf7214e8fdfa2512
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-04-07 02:56:57 +0200

    Added authorization in backend

---
+++ quali-t-app/app/logics/authentication/QualitDeadboltHandler.java
@@ -0,0 +1,43 @@
+package logics.authentication;
+
+import be.objectify.deadbolt.core.models.Subject;
+import be.objectify.deadbolt.java.AbstractDeadboltHandler;
+import be.objectify.deadbolt.java.DeadboltHandler;
+import be.objectify.deadbolt.java.DynamicResourceHandler;
+import dao.models.UserDao;
+import models.authentication.User;
+import play.Logger;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+import play.libs.F;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+
+/**
+ * Created by emre on 06/04/15.
+ */
+public class QualitDeadboltHandler extends AbstractDeadboltHandler {
+    @Override
+    public F.Promise<Result> beforeAuthCheck(Http.Context context) {
+        return F.Promise.pure(null);
+    }
+
+    @Override
+    public Subject getSubject(Http.Context context) {
+        try {
+            return JPA.withTransaction(new play.libs.F.Function0<User>() {
+                public User apply() {
+                    String u = context.session().get("user");
+                    UserDao userDao = new UserDao();
+                    User loggedInUser = userDao.findByUsername(u);
+                    return loggedInUser;
+                }
+            });
+        } catch (Throwable throwable) {
+            play.Logger.error("Error at initializing database with conf/initial-data.yml file");
+        }
+
+        return null;
+    }
+}
