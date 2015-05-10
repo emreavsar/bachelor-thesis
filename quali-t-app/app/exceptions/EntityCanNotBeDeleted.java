252ab795b5cba4e2a4f371a4f14e554c9b59db70
corina.honegger@gmail.com <Dzeta02$>
2015-05-10 20:39:11 +0200

    EntityCanNotBeDeleted Exception

---
+++ quali-t-app/app/exceptions/EntityCanNotBeDeleted.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * Created by corina on 10.05.2015.
+ */
+public class EntityCanNotBeDeleted extends Exception {
+    public EntityCanNotBeDeleted(String message) {
+        super(message);
+    }
+}
