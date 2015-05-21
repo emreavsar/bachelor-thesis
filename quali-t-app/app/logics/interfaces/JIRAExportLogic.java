751e38a5645c3b99829f548e91963cfcd9d02a3b
corina.honegger@gmail.com <Dzeta02$>
2015-05-21 15:33:40 +0200

    Export QA's to JIRA Backend with Exception Handling

--- quali-t-app/app/logics/interfaces/JIRAExportLogic.java
+++ quali-t-app/app/logics/interfaces/JIRAExportLogic.java
@@ -9,10 +9,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
-import dao.interfaces.JIRAConnectionDAO;
 import dao.models.ProjectDAO;
 import dao.models.QAInstanceDAO;
 import exceptions.EntityNotFoundException;
+import exceptions.InvalidConnectionParameter;
+import exceptions.MissingParameterException;
 import models.Interface.JIRAConnection;
 import models.project.Project;
 import models.project.nfritem.Instance;
@@ -32,73 +33,75 @@ import java.util.regex.Pattern;
 
 public class JIRAExportLogic {
     @Inject
-    private JIRAConnectionDAO jiraConnectionDAO;
-    @Inject
     private ProjectDAO projectDAO;
     @Inject
     private QAInstanceDAO qaInstanceDAO;
 
-    public JsonNode exportToJira(Project project, List<Long> qualityAttributesToExport, Boolean exportAsRaw) throws EntityNotFoundException {
-//        JIRAConnection connectionParameter = getConnectionParameter(Long.parseLong("1500"));
-//        Project project = projectDAO.readById(Long.parseLong("11000"));
-//        List<Long> qaIds = new ArrayList<>();
-//        ObjectNode responses = new ObjectNode(JsonNodeFactory.instance);
-////        getDescriptionWithVars(qaInstanceDAO.readById(Long.parseLong("5")));
-//        qaIds.add(Long.parseLong("5"));
-        project = projectDAO.readById(project.getId());
-        JIRAConnection connectionParameter = project.getJiraConnection();
-        ObjectNode responses = new ObjectNode(JsonNodeFactory.instance);
+    public JsonNode exportToJira(Project project, List<Long> qualityAttributesToExport, Boolean exportAsRaw) throws EntityNotFoundException, InvalidConnectionParameter, MissingParameterException {
+        if (project != null && qualityAttributesToExport != null && exportAsRaw != null) {
+            //get required parameters
+            project = projectDAO.readById(project.getId());
+            JIRAConnection connectionParameter = project.getJiraConnection();
+            ObjectNode responses = new ObjectNode(JsonNodeFactory.instance);
 
-        for (Long id : qualityAttributesToExport) {
-            Instance qa = qaInstanceDAO.readById(id);
-            String text = getDescriptionWithVars(qa);
-            if (!exportAsRaw) {
-                text = removeHtmlTags(text);
+            //create each QA as JIRA issue
+            for (Long id : qualityAttributesToExport) {
+                Instance qa = qaInstanceDAO.readById(id);
+                //replace vars with values
+                String text = getDescriptionWithVars(qa);
+                //check if export should be raw or html
+                if (!exportAsRaw) {
+                    text = removeHtmlTags(text);
+                }
+                //parse JSON for JIRA POST
+                JsonNode jsonString = Json.parse("{\n" +
+                        "    \"fields\": {\n" +
+                        "       \"project\":\n" +
+                        "       {\n" +
+                        "          \"key\": \"" + project.getJiraKey() + "\"\n" +
+                        "       },\n" +
+                        "       \"summary\": \"Project " + project.getName() + " / QA " + qa.getId() + "\",\n" +
+                        "       \"description\": \"" + text + "\",\n" +
+                        "       \"issuetype\": {\n" +
+                        "          \"name\": \"Task\"\n" +
+                        "       }\n" +
+                        "   }\n" +
+                        "}");
+                //connect to JIRA connection and POST JSON
+                try {
+                    WSRequestHolder request = WS.url(connectionParameter.getHostAddress() + "/rest/api/latest/issue");
+
+                    WSRequestHolder complexRequest = request.setAuth(connectionParameter.getUsername(), connectionParameter.getPassword(), WSAuthScheme.BASIC)
+                            .setContentType("application/json");
+                    F.Promise<JsonNode> jsonResponse = complexRequest.post(jsonString).map(response -> response.asJson());
+                    JsonNode createIssueResponse = jsonResponse.get(10000);
+
+                    //check if there was an error during creating of issue
+                    if (createIssueResponse.has("errors")) {
+                        throw new InvalidConnectionParameter("Please check your JIRA Key Parameter");
+                    }
+
+                    //persist JIRA Issue Key and Direct URL to database
+                    qa.setJiraKey(createIssueResponse.findPath("key").asText());
+                    qa.setJiraDirectURL(createIssueResponse.findPath("self").asText());
+                    qaInstanceDAO.update(qa);
+
+                    //add jira Response to Json for final response with all QAs
+                    ((ObjectNode) responses).put(qa.getId().toString(), createIssueResponse);
+                } catch (InvalidConnectionParameter e) {
+                    throw e;
+                } catch (Exception e) {
+                    throw new InvalidConnectionParameter("Could not connect to the selected JIRA-Connection!");
+                }
             }
-
-            JsonNode jsonString = Json.parse("{\n" +
-                    "    \"fields\": {\n" +
-                    "       \"project\":\n" +
-                    "       {\n" +
-                    "          \"key\": \"" + project.getJiraKey() + "\"\n" +
-                    "       },\n" +
-                    "       \"summary\": \"Project " + project.getName() + " / QA " + qa.getId() + "\",\n" +
-                    "       \"description\": \"" + text + "\",\n" +
-                    "       \"issuetype\": {\n" +
-                    "          \"name\": \"Task\"\n" +
-                    "       }\n" +
-                    "   }\n" +
-                    "}");
-            WSRequestHolder request = WS.url(connectionParameter.getHostAddress() + "/rest/api/latest/issue");
-
-            WSRequestHolder complexRequest = request.setAuth(connectionParameter.getUsername(), connectionParameter.getPassword(), WSAuthScheme.BASIC)
-                    .setContentType("application/json");
-            F.Promise<JsonNode> jsonResponse = complexRequest.post(jsonString).map(response -> {
-                return response.asJson();
-            });
-            JsonNode createIssueResponse = jsonResponse.get(10000);
-            qa.setJiraKey(createIssueResponse.findPath("key").asText());
-            qa.setJiraDirectURL(createIssueResponse.findPath("self").asText());
-            qaInstanceDAO.update(qa);
-
-            ((ObjectNode) responses).put(qa.getId().toString(), createIssueResponse);
+            return responses;
         }
-        return responses;
+        throw new MissingParameterException("Please provide all required Parameters!");
 
     }
 
-    public JIRAConnection getConnectionParameter(Long id) throws EntityNotFoundException {
-        return jiraConnectionDAO.readById(id);
-    }
-
-    public String getDescriptionWithVars(Instance qualityAttributeInstance) throws EntityNotFoundException {
+    private String getDescriptionWithVars(Instance qualityAttributeInstance) throws EntityNotFoundException {
         Set<Val> values = qualityAttributeInstance.getValues();
-
-//            String number = qa.getDescription().matches("%(.*?)%");
-//            String description = qa.getDescription().replaceFirst("%VARIABLE(.*?)%", "");
-
-//        String description = .replaceFirst("%VARIABLE_.*?([0-9]*)%", "");
-//        Logger.info(description);
         return replacePlaceholder(qualityAttributeInstance.getDescription(), values);
     }
 
