11758481e1ad751c1e9faa3f1cfe95bb01bd6f57
corina.honegger@gmail.com <Dzeta02$>
2015-05-30 23:45:39 +0200

    Return Error fix (Tests)

--- quali-t-app/test/unit/daos/project/UserDaoTest.java
+++ quali-t-app/test/unit/daos/project/UserDaoTest.java
@@ -4,6 +4,7 @@ import base.AbstractDatabaseTest;
 import base.AbstractTestDataCreator;
 import dao.models.UserDao;
 import exceptions.EntityAlreadyExistsException;
+import exceptions.MissingParameterException;
 import models.authentication.User;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,7 +22,7 @@ public class UserDaoTest extends AbstractDatabaseTest {
     }
 
     @Test
-    public void createUserTest() throws EntityAlreadyExistsException {
+    public void createUserTest() throws EntityAlreadyExistsException, MissingParameterException {
         // ARRANGE
         AbstractTestDataCreator.createUser("Hans", "1234");
         // ACT
