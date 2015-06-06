8eaca735637945464ad04121d1f92b3800d2e02b
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-06-06 17:07:04 +0200

    QUALI-256 Rolle im Role Changer nur anzeigen, wenn im Backend die Rolle registriert ist. Plus added project manager

---
+++ quali-t-app/public/app/scripts/controllers/roles.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qualitApp.controller:RolesCtrl
+ * @description
+ * # RolesCtrl
+ * Controller of the qualitApp
+ */
+angular.module('qualitApp')
+  .controller('RolesCtrl', function ($scope, $rootScope) {
+    $scope.tooltipsRoleChanger = "You can change your current role. " +
+    "By changing the current role, the navigation will be adjusted, so that you can work more powerful.";
+
+    $scope.changeRole = function (newRole) {
+      $rootScope.selectedRole = newRole;
+    }
+
+    /**
+     * Checks wether the role is available to change for the user, if not it has no sense to show it to the user
+     * @param role
+     * @returns {boolean}
+     */
+    $scope.isRoleAvailable = function (role) {
+      return $rootScope._identity.roles.indexOf(role) > -1;
+    }
+  });
