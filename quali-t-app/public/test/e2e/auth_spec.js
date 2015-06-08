ba04419a64602a16a54541ce5441070461a9d8c0
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-06-08 14:35:41 +0200

    QUALI-218 Frontend Tests schreiben: Authentication

--- quali-t-app/public/test/e2e/auth_spec.js
+++ quali-t-app/public/test/e2e/auth_spec.js
@@ -1,36 +1,36 @@
 'use strict';
 
 describe('login', function () {
-  //it('should go to login page, try to login with a non existent user', function () {
-  //  browser.get('/#/login');
-  //  expect(browser.getLocationAbsUrl()).toMatch("/login");
-  //
-  //  var usernameInput = element(by.id("input-username"));
-  //  var passwordInput = element(by.id("input-password"));
-  //
-  //  usernameInput.sendKeys("this_username_does_not_exist");
-  //  passwordInput.sendKeys("foobar");
-  //
-  //  element(by.id("button_sign_in")).click();
-  //
-  //  expect(browser.getLocationAbsUrl()).toMatch("/login");
-  //});
-  //
-  //
-  //it('should go to login page, try to login with an existent user', function () {
-  //  browser.get('/#/login');
-  //  expect(browser.getLocationAbsUrl()).toMatch("/login");
-  //
-  //  var usernameInput = element(by.id("input-username"));
-  //  var passwordInput = element(by.id("input-password"));
-  //
-  //  usernameInput.sendKeys("admin@quali-t.ch");
-  //  passwordInput.sendKeys("admin");
-  //
-  //  element(by.id("button_sign_in")).click();
-  //
-  //  expect(browser.getLocationAbsUrl()).toMatch("/authenticated/dashboard");
-  //});
+  it('should go to login page, try to login with a non existent user', function () {
+    browser.get('/#/login');
+    expect(browser.getLocationAbsUrl()).toMatch("/login");
+
+    var usernameInput = element(by.id("input-username"));
+    var passwordInput = element(by.id("input-password"));
+
+    usernameInput.sendKeys("this_username_does_not_exist");
+    passwordInput.sendKeys("foobar");
+
+    element(by.id("button_sign_in")).click();
+
+    expect(browser.getLocationAbsUrl()).toMatch("/login");
+  });
+
+
+  it('should go to login page, try to login with an existent user', function () {
+    browser.get('/#/login');
+    expect(browser.getLocationAbsUrl()).toMatch("/login");
+
+    var usernameInput = element(by.id("input-username"));
+    var passwordInput = element(by.id("input-password"));
+
+    usernameInput.sendKeys("admin@quali-t.ch");
+    passwordInput.sendKeys("admin");
+
+    element(by.id("button_sign_in")).click();
+
+    expect(browser.getLocationAbsUrl()).toMatch("/authenticated/dashboard");
+  });
 
   it('should try to register a new user', function () {
     browser.get('/#/register');
