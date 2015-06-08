18cb5f7a5a1d62bdd1942730f99220e034b519ea
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-06-08 22:50:59 +0200

    added sort and alerts to jiraconnections admin

--- quali-t-app/public/app/scripts/controllers/jiraconnection.js
+++ quali-t-app/public/app/scripts/controllers/jiraconnection.js
@@ -8,26 +8,49 @@
  * Controller of the qualitApp
  */
 angular.module('qualitApp')
-  .controller('JiraconnectionCtrl', function ($scope, apiService, $modal) {
+  .controller('JiraconnectionCtrl', function ($scope, apiService, $modal, ngTableParams, $filter, alertService) {
     $scope.name = "";
     $scope.description = "";
-    $scope.errors = new Array();
-    $scope.success = new Array();
 
     $scope.loadJIRAConnections = function () {
       var promise = apiService.loadJIRAConnections();
       promise.then(function (payload) {
         $scope.jiraConnectionList = payload.data;
+        if ($scope.tableParams == undefined) {
+          $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 10,
+            sorting: {
+              id: 'asc'
+            }
+          }, {
+            total: function () {
+              return $scope.jiraConnectionList.length;
+            },
+            getData: function ($defer, params) {
+              // use build-in angular filter
+              var orderedData = params.sorting() ?
+                $filter('orderBy')($scope.jiraConnectionList, params.orderBy()) :
+                $scope.jiraConnectionList;
+              params.total(orderedData.length);
+              $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+          });
+        }
+        else {
+          $scope.tableParams.reload();
+        }
+
       });
     }
 
     $scope.delete = function (jiraconnection) {
-      var conf = confirm('This will delete all projects etc. of the qp, u sure?');
+      var conf = confirm('This will delete all connections inside projects to this JIRA connection, are you sure you want to do this?');
       if (conf) {
         var deletePromise = apiService.deleteJiraConnection(jiraconnection.id);
         deletePromise.then(function (payload) {
           $scope.jiraConnectionList = payload.data;
-          // todo: is this needed?
+          alertService.createSuccess("JIRA connection successfully deleted");
           $scope.loadJIRAConnections();
         });
       }
@@ -37,8 +60,8 @@ angular.module('qualitApp')
       var editPromise = apiService.editJIRAConnection(jiraconnection.id, jiraconnection.hostAddress, jiraconnection.username, jiraconnection.password);
 
       editPromise.then(function (payload) {
+        alertService.createSuccess("JIRA connection successfully updated");
         $scope.loadJIRAConnections();
-        $scope.success.push(payload.data);
       });
     }
 
@@ -59,8 +82,8 @@ angular.module('qualitApp')
     $scope.addJIRAConnection = function (jiraconnection) {
       var addPromise = apiService.addJIRAConnection(jiraconnection.hostAddress, jiraconnection.username, jiraconnection.password);
       addPromise.then(function (payload) {
+        alertService.createSuccess("JIRA connection successfully added");
         $scope.loadJIRAConnections();
-        $scope.success.push(payload.data);
       });
     }
 
