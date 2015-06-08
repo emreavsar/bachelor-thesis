12f4c628d94ed1b5e782a2e0b765dd2edb28a311
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-06-08 22:45:34 +0200

    added sort and alerts to qp admin

--- quali-t-app/public/app/scripts/controllers/qualityproperty.js
+++ quali-t-app/public/app/scripts/controllers/qualityproperty.js
@@ -8,7 +8,7 @@
  * Controller of the qualitApp
  */
 angular.module('qualitApp')
-  .controller('QualitypropertyCtrl', function ($scope, apiService, $modal) {
+  .controller('QualitypropertyCtrl', function ($scope, apiService, $modal, ngTableParams, $filter, alertService) {
     $scope.name = "";
     $scope.description = "";
     $scope.errors = new Array();
@@ -18,17 +18,43 @@ angular.module('qualitApp')
       var loadPromise = apiService.getQualityProperties();
       loadPromise.then(function (payload) {
         $scope.qpList = payload.data;
+
+        if ($scope.tableParams == undefined) {
+          $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 10,
+            sorting: {
+              id: 'desc'
+            }
+          }, {
+            total: function () {
+              return $scope.qpList.length;
+            },
+            getData: function ($defer, params) {
+              // use build-in angular filter
+              var orderedData = params.sorting() ?
+                $filter('orderBy')($scope.qpList, params.orderBy()) :
+                $scope.qpList;
+              params.total(orderedData.length);
+              $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+          });
+        }
+        else {
+          $scope.tableParams.reload();
+        }
       });
     }
 
 
     $scope.delete = function (qp) {
-      var conf = confirm('This will delete all projects etc. of the qp, u sure?');
+      var conf = confirm('This will delete all states (in quality attributes inside projects) connected to this qp, are u sure you want to do this?');
       if (conf) {
         var deletePromise = apiService.deleteQualityPropery(qp.id);
         deletePromise.then(function (payload) {
           $scope.qpList = payload.data;
           $scope.loadQualityProperties();
+          alertService.createSuccess("Qualityproperty successfully deleted");
         });
       }
     }
@@ -39,7 +65,7 @@ angular.module('qualitApp')
       updatePromise.then(function (payload) {
         $scope.loadQualityProperties();
         $scope.success.push(payload.data);
-        // todo add success alert
+        alertService.createSuccess("Qualityproperty successfully updated");
       });
     }
 
@@ -62,7 +88,7 @@ angular.module('qualitApp')
       createPromise.then(function (payload) {
         $scope.qpList = payload.data;
         $scope.loadQualityProperties();
-        // todo add success alert
+        alertService.createSuccess("Qualityproperty successfully created");
       });
     }
 
