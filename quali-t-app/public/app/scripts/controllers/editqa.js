56621c104fd8621960df647d8ab9b708c8683825
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-06-09 13:37:00 +0200

    QUALI-262 ausfüllen von einem enumnumber mit einem text führt zu einer exception

--- quali-t-app/public/app/scripts/controllers/editqa.js
+++ quali-t-app/public/app/scripts/controllers/editqa.js
@@ -8,7 +8,7 @@
  * Controller of the qualitApp
  */
 angular.module('qualitApp')
-  .controller('EditQaCtrl', function ($scope, apiService, $rootScope) {
+  .controller('EditQaCtrl', function ($scope, apiService, $rootScope, alertService) {
     if ($scope.context == 'editproject') {
       $scope.isTextMode = false;
     } else if ($scope.context == 'editcatalog') {
@@ -26,78 +26,84 @@ angular.module('qualitApp')
       $scope.hideModal = hideFunction;
     }
 
+    $scope.isFormValid = function () {
+      return $("input[type='number']:invalid").size() == 0;
+    };
+
     /**
      * Updates the quality attribute instance. Either the text or the values in the variable
      */
     $scope.update = function () {
-      var instance = $scope.qa;
-      // delete not needed information
-      delete instance.qualityPropertyStatus;
-      delete instance.template;
+      if ($scope.isFormValid()) {
+        var instance = $scope.qa;
+        // delete not needed information
+        delete instance.qualityPropertyStatus;
+        delete instance.template;
 
-      // update QAs text
-      if ($scope.isTextMode) {
-        instance.description = $scope.qaNew.description;
-      } else {
-        // update QAs instance values
-        var qaValues = new Array();
+        // update QAs text
+        if ($scope.isTextMode) {
+          instance.description = $scope.qaNew.description;
+        } else {
+          // update QAs instance values
+          var qaValues = new Array();
 
-        // get all inputs, selects etc. where variable can be inserted by user
-        var varInputs = $(".modal .qa:visible").find("[data-for-variable]");
-        for (var i = 0; i < varInputs.length; i++) {
-          var varInput = varInputs[i];
-          var valueId = $(varInput).data("value-id");
-          var valueVarIndex = $(varInput).data("value-varindex");
-          var valueUserInput = "";
+          // get all inputs, selects etc. where variable can be inserted by user
+          var varInputs = $(".modal .qa:visible").find("[data-for-variable]");
+          for (var i = 0; i < varInputs.length; i++) {
+            var varInput = varInputs[i];
+            var valueId = $(varInput).data("value-id");
+            var valueVarIndex = $(varInput).data("value-varindex");
+            var valueUserInput = "";
 
-          var isExtendable = $(varInput).parent().hasClass("isExtendable");
-          var ignore = false;
+            var isExtendable = $(varInput).parent().hasClass("isExtendable");
+            var ignore = false;
 
-          // if input, get value
-          if ($(varInput).is("input")) {
-            if (isExtendable) {
-              // if the input text is an extendable and has a value, take it otherwise the dropdown will be taken
-              if ($(varInput).val() != "") {
+            // if input, get value
+            if ($(varInput).is("input")) {
+              if (isExtendable) {
+                // if the input text is an extendable and has a value, take it otherwise the dropdown will be taken
+                if ($(varInput).val() != "") {
+                  valueUserInput = $(varInput).val();
+                } else {
+                  ignore = true;
+                }
+              } else {
                 valueUserInput = $(varInput).val();
-              } else {
-                ignore = true;
               }
-            } else {
-              valueUserInput = $(varInput).val();
-            }
-          } else if ($(varInput).is("select")) {// if select get selected option
-            // check if extendable
-            if (isExtendable) {
-              // check if the input text has a value inside, if yes -> ignore this one
-              if ($(varInput).parent().find("input").val() == "") {
+            } else if ($(varInput).is("select")) {// if select get selected option
+              // check if extendable
+              if (isExtendable) {
+                // check if the input text has a value inside, if yes -> ignore this one
+                if ($(varInput).parent().find("input").val() == "") {
+                  valueUserInput = $(varInput).find(":selected").val();
+                } else {
+                  ignore = true;
+                }
+              } else {
                 valueUserInput = $(varInput).find(":selected").val();
-              } else {
-                ignore = true;
               }
-            } else {
-              valueUserInput = $(varInput).find(":selected").val();
+            }
+
+            if (!ignore) {
+              var value = {
+                varIndex: valueVarIndex,
+                value: valueUserInput
+              }
+
+              // add id only if new
+              if (valueId != "") {
+                value.id = valueId;
+              }
+              qaValues.push(value);
             }
           }
 
-          if (!ignore) {
-            var value = {
-              varIndex: valueVarIndex,
-              value: valueUserInput
-            }
-
-            // add id only if new
-            if (valueId != "") {
-              value.id = valueId;
-            }
-            qaValues.push(value);
-          }
+          instance.values = qaValues;
         }
+        apiService.updateQaInstance(instance);
+        $rootScope.$broadcast('projectsQaUpdated', {});
 
-        instance.values = qaValues;
+        $scope.hideModal();
       }
-      apiService.updateQaInstance(instance);
-      $rootScope.$broadcast('projectsQaUpdated', {});
-
-      $scope.hideModal();
     }
   });
