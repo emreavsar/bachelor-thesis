a5c68e76182fb65ab610ed8b45b4638885f26e5e
emre.avsar92@gmail.com <emre.avsar92@gmail.com>
2015-06-11 16:55:25 +0200

    fixed nullpointer when there are no variables or template for qa in statistics

--- quali-t-app/app/logics/validation/StatisticService.java
+++ quali-t-app/app/logics/validation/StatisticService.java
@@ -23,40 +23,43 @@ public class StatisticService implements ValidationService {
 
         for (Instance qa : qualityAttributes) {
 
-            // order values of the instance by varIndex
-            ArrayList<Val> orderedValues = new ArrayList(qa.getValues());
-            Collections.sort(orderedValues, (v1, v2) -> NumberUtils.compare(v1.getVarIndex(), v2.getVarIndex()));
+            // statistics work only for variables
+            if (qa.getTemplate() != null && qa.getTemplate().getVariables() != null && qa.getTemplate().getVariables().size() > 0) {
+                // order values of the instance by varIndex
+                ArrayList<Val> orderedValues = new ArrayList(qa.getValues());
+                Collections.sort(orderedValues, (v1, v2) -> NumberUtils.compare(v1.getVarIndex(), v2.getVarIndex()));
 
-            // order values of the instance by varIndex
-            ArrayList<QAVar> orderedVariables = new ArrayList(qa.getTemplate().getVariables());
-            Collections.sort(orderedVariables, (v1, v2) -> NumberUtils.compare(v1.getVarIndex(), v2.getVarIndex()));
+                // order values of the instance by varIndex
+                ArrayList<QAVar> orderedVariables = new ArrayList(qa.getTemplate().getVariables());
+                Collections.sort(orderedVariables, (v1, v2) -> NumberUtils.compare(v1.getVarIndex(), v2.getVarIndex()));
 
-            // iterate through vals and compare them with CatalogQA's QAVar
-            for (int i = 0; i < orderedValues.size(); i++) {
-                Val val = orderedValues.get(i);
-                QAVar var = orderedVariables.get(i);
-                ArrayList<String> warnings = new ArrayList<>();
+                // iterate through vals and compare them with CatalogQA's QAVar
+                for (int i = 0; i < orderedValues.size(); i++) {
+                    Val val = orderedValues.get(i);
+                    QAVar var = orderedVariables.get(i);
+                    ArrayList<String> warnings = new ArrayList<>();
 
-                // do not check freetext
-                if (!var.getType().equals(QAType.FREETEXT)) {
-                    // average for numbers
-                    if (var.getAverageValue() != null && (var.getType().equals(QAType.ENUMNUMBER) || var.getType().equals(QAType.FREENUMBER))) {
-                        Double avg = Double.valueOf(var.getAverageValue());
-                        Double value = Double.valueOf(val.getValue());
+                    // do not check freetext
+                    if (!var.getType().equals(QAType.FREETEXT)) {
+                        // average for numbers
+                        if (var.getAverageValue() != null && (var.getType().equals(QAType.ENUMNUMBER) || var.getType().equals(QAType.FREENUMBER))) {
+                            Double avg = Double.valueOf(var.getAverageValue());
+                            Double value = Double.valueOf(val.getValue());
 
-                        // warn if value is more then average +- max_deviation
-                        if (value > avg * (1 + MAX_DEVIATION) || value < avg * (1 - MAX_DEVIATION)) {
-                            warnings.add("The average value (over all projects) for this quality attribute is "
-                                    + avg + ", but you specified " + value + " which has an deviation of " + MAX_DEVIATION * 100 + "%");
+                            // warn if value is more then average +- max_deviation
+                            if (value > avg * (1 + MAX_DEVIATION) || value < avg * (1 - MAX_DEVIATION)) {
+                                warnings.add("The average value (over all projects) for this quality attribute is "
+                                        + avg + ", but you specified " + value + " which has an deviation of " + MAX_DEVIATION * 100 + "%");
+                            }
                         }
-                    }
-                    // mostused for all
-                    if (var.getMosteUsedValue() != null && !var.getMosteUsedValue().equals(val.getValue())) {
-                        warnings.add("The most used value (over all projects) for this quality attribute is "
-                                + var.getMosteUsedValue() + ", but you specified " + val.getValue() + ".");
-                    }
-                    if (warnings.size() > 0) {
-                        validationWarnings.put(qa.getId(), warnings);
+                        // mostused for all
+                        if (var.getMosteUsedValue() != null && !var.getMosteUsedValue().equals(val.getValue())) {
+                            warnings.add("The most used value (over all projects) for this quality attribute is "
+                                    + var.getMosteUsedValue() + ", but you specified " + val.getValue() + ".");
+                        }
+                        if (warnings.size() > 0) {
+                            validationWarnings.put(qa.getId(), warnings);
+                        }
                     }
                 }
             }
